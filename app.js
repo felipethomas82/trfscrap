ac3d8ecf0bb67c3ecfe60142ec6e6d71f4959b8f
Felipe Thomas <felipethomas@Felipes-MacBook-Pro.local>
2019-12-11 23:36:54 -0300

    Production version

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ function insertScrape( paths ) {
         client.close();
     })
 }
-
+// db.toScrap.count({"scraped": false})
 /*
     Function to get the paths to execute the scraping 
 */
@@ -70,6 +70,31 @@ async function getToScrap() {
 }
 
 /*
+    Function to get the count of pages to Scrap
+*/
+async function getCountToScrap() {
+    const client = await mongo.connect(mongoUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).catch(err => console.log(err));
+    
+    if (!client) {
+        console.error(err)
+        return
+    }
+    try {
+        const db = client.db(DB_NAME);
+        const collection = db.collection(TO_SCRAP);
+        let count = await collection.count({"scraped": false});
+        return count;
+    } catch (error) {
+        console.log(error);
+    } finally {
+        client.close();
+    }
+}
+
+/*
     Function to get the paths to execute the scraping 
 */
 async function updatePath( path, scraped ) {
@@ -85,12 +110,49 @@ async function updatePath( path, scraped ) {
     try {
         const db = client.db(DB_NAME);
         const collection = db.collection(TO_SCRAP);
-        let result = await collection.update( {"path" : path }, {"scraped": scraped});
+        let result = await collection.updateOne( {"path" : path }, {$set : {"scraped": scraped}});
     } catch (error) {
         console.log(error);
     } finally {
         client.close();
     }
+}
+
+function remainingTime( total, totalToScrap, i ) {
+    let remaining = (((total/i) * (totalToScrap - i))/1000);
+    return remaining > 3600 ? (remaining/60/60).toFixed(2) + " hrs" : 
+            remaining > 60 ? (remaining/60).toFixed(2) + " mins" :
+            remaining.toFixed(2) + " secs";
+}
+
+/*
+    Function to get the number of 'gproc' or 'documento' inside the path string.
+    The path to scrap can have different formats, so the string gproc is not 
+    present in all of them. I discovered this after a while :/
+    Found 'numeroProcesso' too. :S
+    Has PDF too. Not processed for now.
+    Examples: 
+    https://jurisprudencia.trf4.jus.br/pesquisa/inteiro_teor.php?orgao=1&documento=9442770&termosPesquisados=J2Fwb3NlbnRhZG9yaWEgZXNwZWNpYWwnICdtb3RvcmlzdGEgZGUgb25pYnVzJw==
+    https://jurisprudencia.trf4.jus.br/pesquisa/inteiro_teor.php?orgao=1&numero_gproc=40000850053&versao_gproc=4&crc_gproc=5fba9ca6&termosPesquisados=J2Fwb3NlbnRhZG9yaWEgZXNwZWNpYWwnICdtb3RvcmlzdGEgZGUgb25pYnVzJw==
+    https://jurisprudencia.trf4.jus.br/pesquisa/inteiro_teor.php?orgao=1&numeroProcesso=200471020050973&dataDisponibilizacao=18/05/2007
+    http://jurisprudencia.trf4.jus.br/pesquisa/inteiro_teor.php?arquivo=/trf4/volumes2/VOL0059/20031015/ST5/3062003/200004011332189A.0664.PDF
+
+*/
+function getProcNumber( pathToScrap ) {
+    if (pathToScrap.indexOf("numero_gproc=") >= 0) {
+        return pathToScrap.split("numero_gproc=")[1].split("&")[0];
+    } 
+    if (pathToScrap.indexOf("documento=") >= 0) {
+        return pathToScrap.split("documento=")[1].split("&")[0] + "_d";
+    }
+    if (pathToScrap.indexOf("numeroProcesso=") >= 0) {
+        return pathToScrap.split("numeroProcesso=")[1].split("&")[0] + "_np";
+    }
+    if (pathToScrap.indexOf("arquivo=") >= 0) {
+        pathToScrap = pathToScrap.split("arquivo=")[1].split("/");
+        return pathToScrap[pathToScrap.length - 1];
+    }
+    
 }
 
 (async () => {
@@ -124,7 +186,6 @@ async function updatePath( path, scraped ) {
         if ( content.indexOf( "frmDocumentos" ) > -1) {    
             await page.click('#parcial').catch(err => console.log(err));
         } 
-        // const teste = await page.$$('a');
         let tryNext = true;
         let pageNumber = 1;
         // while has pagination, will try the next result page
@@ -157,46 +218,55 @@ async function updatePath( path, scraped ) {
     
     // after the crawler completes the job, it begins to scraping the pages and saves the results to a folder
     let pathToScrap = await getToScrap();
+    let totalToScrap = await getCountToScrap();
+    let start;
+    let end;
+    let total = 0;
+    let i = 1;
     while(pathToScrap) {
-        console.log("Saving the html for the path: " + pathToScrap);
-        let nroGProc = pathToScrap.split("numero_gproc=")[1].split("&")[0];
+        start = new Date(); //get current date to measurement
+        console.log("\nScraping " + i + " from " + totalToScrap + " pages to scrap");
+        console.log("Scraping the path: " + pathToScrap); 
+        // let nroGProc = pathToScrap.split("numero_gproc=")[1].split("&")[0];
+        let nroGProc = getProcNumber( pathToScrap );
         console.log("numero_gproc: " + nroGProc);
-        
-        const response = await page.goto(pathToScrap, {waitUntil: 'load'});
-        // await page.setContent((await response.buffer()).toString('ISO-8859-1'));
-        // const innerText = await page.evaluate(() => document.querySelector('body').innerText);
-        const html = await page.content();
-        fs.writeFileSync('scraped/' + nroGProc + '.html', html);
-        // fs.writeFileSync('scraped/' + nroGProc + '2.txt', innerText);
-
-        // updatePath(pathToScrap);
+        try {
+            if (pathToScrap.toLowerCase().indexOf(".pdf") >= 0) {
+                updatePath(pathToScrap, true); //sets true to remove from list
+                i++;
+                console.log("**** PDF -> Skip *****");
+                continue;
+            }
+            const response = await page.goto(pathToScrap, {waitUntil: 'load'});
+            // await page.setContent((await response.buffer()).toString('utf8'));
+            const innerText = await page.evaluate(() => document.querySelector('body').innerText);
+            const html = await page.content();
+            if (innerText.indexOf("Acesso negado") >= 0) {
+                console.log("******** ACCESS DENIED. Try again later **************");
+                continue;
+            }
+            //save the result in html and txt format
+            console.log("Saving html file at scraped/" + nroGProc + ".html");
+            fs.writeFileSync('scraped/' + nroGProc + '.html', html);
+            console.log("Saving text file at scraped/" + nroGProc + ".txt");
+            fs.writeFileSync('scraped/' + nroGProc + '.txt', innerText);
+            updatePath(pathToScrap, true); //sets true to remove from list
+            //get another path to scrap
+            pathToScrap = await getToScrap();
+            //show the total time to scrap and remaining time
+            end = new Date() - start;
+            total += end;
+            console.log("Scraping time: " + (end/1000).toFixed(4) + "s");
+            console.log("Total execution time: " + (total/1000).toFixed(4) + "s");
+            console.log("Remaining time: " + remainingTime(total, totalToScrap, i));
+            i++;
+        } catch (error) {
+            console.log("**** Timeout. Trying again. *****");
+        }
     }
+    console.log("---------------------------------------------------");
+    console.log("Closing headless browser. Ending connection.");
     await browser.close();
-    /*
-    // get the first page to fill the search input
-    let hotelData = await page.evaluate(() => {
-        let hotels = [];
-        // get the hotel elements
-        let hotelsElms = document.querySelectorAll('div.sr_property_block[data-hotelid]');
-        // get the hotel data
-        hotelsElms.forEach((hotelelement) => {
-            let hotelJson = {};
-            try {
-                hotelJson.name = hotelelement.querySelector('span.sr-hotel__name').innerText;
-                hotelJson.reviews = hotelelement.querySelector('span.review-score-widget__subtext').innerText;
-                hotelJson.rating = hotelelement.querySelector('span.review-score-badge').innerText;
-                if(hotelelement.querySelector('strong.price')){
-                    hotelJson.price = hotelelement.querySelector('strong.price').innerText;
-                }
-            }
-            catch (exception){
-
-            }
-            hotels.push(hotelJson);
-        });
-        return hotels;
-    });
-    */
-
-    // console.dir(hotelData);
+    console.log("END SCRAPING PROGRAM");
+    console.log("\n\n");
 })();
